d1a2c71a1d1b2920c6651ab38a25b50adb0e7587
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-09-12 08:26:50 +0200

    Add connection script to verify server functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  return <div className="text-4xl font-bold">Hello World</div>;
+  const [fetchData, setFetchData] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:5001/")
+      .then((response) => response.json())
+      .then((data) => setFetchData(data.message));
+  }, []);
+
+  return (
+    <div className="text-5xl font-bold bg-rose-500 text-white p-10">
+      {fetchData ? fetchData : "SERVER CONNECTION ERROR"}
+    </div>
+  );
 };
 
 export default App;
