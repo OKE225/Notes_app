27bf3ea477f695c973900c0477ff28541d5fd0cc
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-09-22 19:14:45 +0200

    Add page to view and edit note content

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import AddNote from "./pages/AddNote";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router";
+import NoteDetails from "./pages/NoteDetails";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/add" element={<AddNote />} />
+      <Route path="/note/:id" element={<NoteDetails />} />
     </Routes>
   );
 };
--- client/src/components/notes/NoteCard.tsx
+++ client/src/components/notes/NoteCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import type Note from "../../types/note";
 
 interface Props {
@@ -6,10 +7,12 @@ interface Props {
 
 const NoteCard = ({ note }: Props) => {
   return (
-    <div className="bg-blue-500 hover:bg-blue-600 shadow-lg p-4 rounded-lg cursor-pointer text-white font-light">
+    <Link
+      to={`/note/${note._id}`}
+      className="bg-blue-500 hover:bg-blue-600 shadow-lg p-4 rounded-lg cursor-pointer text-white font-light">
       <h1 className="text-3xl mb-2">{note.title}</h1>
       <p className="text-md">{note.content}</p>
-    </div>
+    </Link>
   );
 };
 
