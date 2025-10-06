ed8debf1339d11f9ebb8043bf99287edf341c33e
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-10-06 21:59:04 +0200

    Add content to button

--- client/src/components/layout/HomeSidebar.tsx
+++ client/src/components/layout/HomeSidebar.tsx
@@ -3,11 +3,11 @@ import { IoIosAdd } from "react-icons/io";
 
 const HomeSidebar = () => {
   return (
-    <aside className="w-[100px] min-h-screen border-r-2 bg-stone-100 border-r-stone-200 text-center py-10 flex flex-col items-center">
+    <aside className="w-[135px] min-h-screen border-r-2 bg-stone-100 border-r-stone-200 text-center py-5 flex flex-col items-center">
       <Link
         to="/add"
-        className="bg-blue-500 hover:bg-blue-600 text-white p-1 cursor-pointer rounded-full flex justify-center shadow-lg">
-        <IoIosAdd className="text-3xl" />
+        className="bg-blue-500 hover:bg-blue-600 text-white px-5 cursor-pointer rounded-full flex items-center justify-center shadow-lg py-1">
+        <IoIosAdd className="-ml-2 text-2xl" /> Add
       </Link>
     </aside>
   );
